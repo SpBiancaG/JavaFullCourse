6dacd1760532617fd3d1f7342c6f33ec7d34470f
Bianca <spbiancag@gmail.com>
2023-07-10 12:48:48 +0300

    Playing with Math class functions

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-      String name= JOptionPane.showInputDialog("Enter your name");
-      JOptionPane.showMessageDialog(null,"Hello "+name);
-      //input dialog has by default a string input, so we need to convert
-      int age=Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
-      JOptionPane.showMessageDialog(null, "You are "+age+" y.o");
+   double x=3.14;
+   double y= 10;
 
-      double credit=Double.parseDouble(JOptionPane.showInputDialog("Enter your credit score"));
-        JOptionPane.showMessageDialog(null,"We got your credit score "+credit);
+   double maxi=Math.max(x,y);
+        System.out.println("Max value is "+maxi);
+
+   double mini=Math.min(x,y);
+        System.out.println("Min value is "+mini);
+
+   double absoluteVal=Math.abs(y);//module of a number
+        System.out.println("Absolute value "+absoluteVal);
+
+   double squareRoot=Math.sqrt(y);
+        System.out.println("Square root value is "+squareRoot);
+
+   double round=Math.round(x);
+        System.out.println("Rounded value is "+round);
+
+   double a;
+   double b;
+   double c;
+   Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter side x: ");
+        a=scanner.nextDouble();
+
+        System.out.println("Enter side y: ");
+        b=scanner.nextDouble();
+        c=Math.sqrt((x*x)+(y*y));
+        System.out.println("Hypotenuse is "+c);
+
+        scanner.close();
+
 
     }
 }
