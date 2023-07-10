167f9d45b4f59fa79081ed46c02229aabc751d36
Bianca <spbiancag@gmail.com>
2023-07-10 15:25:17 +0300

    Arrays and usefull string methods

--- src/Main.java
+++ src/Main.java
@@ -4,49 +4,47 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-    Scanner scanner=new Scanner(System.in);
-    String name="";
-    String surname="";
+        //arrays
+        String [] cars={"Camaro","Corvette","Tesla"};
+        cars[0]="Mustang";
+        System.out.println(cars[0]);
 
-    while(name.isBlank())
-    {
-        System.out.println("Enter name ");
-        name=scanner.nextLine();
-    }
+        String[] cars2=new String[3];
 
-
-    do{
-        System.out.println("Enter your surname ");
-        surname=scanner.nextLine();
-    }while (surname.isBlank());
-
-        System.out.println("Hello "+name+" "+surname);
-
-    //for loop
-        for(int i=0;i<=10;i++)
+        for (int i=0;i<cars.length;i++)
         {
-            System.out.println(i);
+            System.out.println(cars[i]);
         }
 
-    //nested
-        Scanner scan=new Scanner(System.in);
-        int rows, columns;
-        String symbol=" ";
+        // 2D arrays
+        String[][] cars3=new String[3][3];
+        cars3[0][0]="Camaro";
 
-        System.out.println("Enter # of rows ");
-        rows=scan.nextInt();
-        System.out.println("Enter 3 of columns ");
-        columns=scan.nextInt();
-        System.out.println("Enter symbol to use");
-        symbol=scan.next();
-
-        for(int i=1;i<=rows; i++)
-        {   System.out.println();
-            for(int j=1;j<=columns;j++)
+        for(int i=0;i<cars3.length;i++){
+            System.out.println();
+            for(int j=0;j<cars3[i].length;j++)
             {
-                System.out.print(symbol);
+                System.out.println(cars3[i][j]+" ");
             }
         }
+
+
+        //String methods
+        String name="Yo";
+        boolean r1=name.equals("Yo");//compares 2 strings and returns true or false
+        int r2=name.length();//length of the string
+        char r3=name.charAt(0);//the character at a certain position in the string
+        int r4=name.indexOf("o");//returns the index of  a certain letter
+        boolean r5=name.isEmpty();//if the string is null
+        String r6=name.toUpperCase();
+        String r7=name.toLowerCase();
+        String r8=name.trim();//kills all the empty spaces in my string
+        String result=name.replace('o','a');
+
+
+
+
+
 
     }
 }
