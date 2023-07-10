51330cf4800afde772965b548ecdd3d32033f2b6
Bianca <spbiancag@gmail.com>
2023-07-10 12:25:08 +0300

    Playing with JOptionPane

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,13 @@ public class Main {
     public static void main(String[] args) {
 
       String name= JOptionPane.showInputDialog("Enter your name");
-      JOptionPane.showMessageDialog(null,"Hello"+name);
+      JOptionPane.showMessageDialog(null,"Hello "+name);
+      //input dialog has by default a string input, so we need to convert
+      int age=Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+      JOptionPane.showMessageDialog(null, "You are "+age+" y.o");
 
+      double credit=Double.parseDouble(JOptionPane.showInputDialog("Enter your credit score"));
+        JOptionPane.showMessageDialog(null,"We got your credit score "+credit);
 
     }
 }
